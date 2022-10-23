e940434b9ab7ec71e12a64b2a83fa485a9276797
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-10-24 01:05:44 +0300

    Step param for iterator

--- datetime_itertool/datetime_iterator.py
+++ datetime_itertool/datetime_iterator.py
@@ -1,22 +1,49 @@
 import datetime
+from typing import Protocol
 
 
-class DateTimeIterator(object):
+class DateTimeIteratorInterface(Protocol):
+    """Interface for datetime iterators."""
+
+    def __iter__(self) -> 'DateTimeIteratorInterface':
+        """Iterator entrypoint.
+
+        :return: DateTimeIterator
+        """
+        raise NotImplementedError
+
+    def __next__(self) -> datetime.datetime:
+        """Iteration next value.
+
+        :return: datetime.datetime
+        :raises StopIteration: if iteration is ended
+        """
+        raise NotImplementedError
+
+
+_default_step = datetime.timedelta(days=1)
+
+
+class DateTimeIterator(DateTimeIteratorInterface):
     """Iterating by time."""
+
+    _shift = datetime.timedelta(days=0)
 
     def __init__(
         self,
         start_datetime: datetime.datetime,
         finish_datetime: datetime.datetime,
+        step: datetime.timedelta = _default_step,
     ):
         """Class constructor.
 
         :param start_datetime: datetime.datetime
         :param finish_datetime: datetime.datetime
+        :param step: datetime.timedelta
         """
         self._start_datetime = start_datetime
         self._finish_datetime = finish_datetime
-        self._shift = datetime.timedelta(days=0)
+        self._step = step
 
     def __iter__(self):
         """Iterator entrypoint.
@@ -31,8 +58,10 @@ class DateTimeIterator(object):
         :return: datetime.datetime
         :raises StopIteration: if iteration is ended
         """
-        iteration_value = self._start_datetime + self._shift
-        if iteration_value == self._finish_datetime:
+        if self._finish_datetime <= self._start_datetime:
             raise StopIteration
-        self._shift += datetime.timedelta(days=1)
+        iteration_value = self._start_datetime + self._shift
+        if iteration_value >= self._finish_datetime:
+            raise StopIteration
+        self._shift += self._step  # noqa: WPS601 rewrite class attribute
         return iteration_value
--- tests/test_datetime_iteration.py
+++ tests/test_datetime_iteration.py
@@ -1,5 +1,7 @@
 import datetime
 
+import pytest
+
 from datetime_itertool.datetime_iterator import DateTimeIterator
 
 
@@ -8,7 +10,38 @@ def test():
     got = list(DateTimeIterator(
         datetime.datetime(2022, 2, 7),
         datetime.datetime(2022, 10, 23),
+        step=datetime.timedelta(days=1),
     ))
 
     assert got[0] == datetime.datetime(2022, 2, 7)
     assert got[-1] == datetime.datetime(2022, 10, 22)
+    assert len(got) == 258
+
+
+@pytest.mark.parametrize(('start', 'end', 'step', 'expect_end', 'count'), [
+    (
+        datetime.datetime(2022, 2, 7),
+        datetime.datetime(2022, 10, 23),
+        datetime.timedelta(days=3),
+        datetime.datetime(2022, 10, 20),
+        86,
+    ),
+])
+def test_with_step(start, end, step, expect_end, count):
+    """Test with step."""
+    got = list(DateTimeIterator(start, end, step))
+
+    assert got[0] == start
+    assert got[-1] == expect_end
+    assert len(got) == count
+
+
+def test_end_less_then_start():
+    """Test end of iteration less than it start."""
+    got = list(DateTimeIterator(
+        datetime.datetime(2022, 10, 23),
+        datetime.datetime(2022, 2, 7),
+        step=datetime.timedelta(days=1),
+    ))
+
+    assert got == []  # noqa: WPS520 check test result is empty list
